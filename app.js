cbc06e614f1df93481c8467a354aa99b917202e5
Seb_105 <28schapfel1@gmail.com>
2023-12-04 08:09:01 -0500

    h

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/", function (req, res) {
   // eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
-app.get("/docs/*", function (req, res) {
+app.get("/docs/", function (req, res) {
   // eslint-disable-next-line no-undef
   res.sendFile(path.join(__dirname + "/public/docs/index.html"));
 });
